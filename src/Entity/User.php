2454e3240e25750425dc535d11ffb5585028e734
Aurel <aurelienwalter@orange.fr>
2020-12-09 20:45:26 +0100

    login logout register with token and mail

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -52,7 +54,7 @@ class User implements UserInterface
     private $username;
 
     /**
-     * @ORM\OneToOne(targetEntity=adresse::class, inversedBy="user", cascade={"persist", "remove"})
+     * @ORM\OneToOne(targetEntity=Adresse::class, inversedBy="user", cascade={"persist", "remove"})
      */
     private $adresse_id;
 
@@ -85,6 +87,11 @@ class User implements UserInterface
      * @ORM\OneToMany(targetEntity=CommentaireUser::class, mappedBy="user_id")
      */
     private $commentaireUsers;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -330,6 +337,18 @@ class User implements UserInterface
                 $commentaireUser->setUserId(null);
             }
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
